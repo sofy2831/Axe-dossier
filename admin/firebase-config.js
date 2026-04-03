13ece63c18f5b69a4064f1ce3bc66f44f22c491e
sofy2831 <s.dumas974@orange.fr>
2026-04-03 14:15:12 +0200

    Update firebase-config.js

--- admin/firebase-config.js
+++ admin/firebase-config.js
@@ -1,4 +1,4 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";xxx
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
